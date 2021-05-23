90e2ea5327b8333b41af0eb0172983adfe520fc6
Vaishali Vyas <vaishalivyas4@users.noreply.github.com>
2021-05-23 17:47:57 +0530

    Commit#1

---
+++ Assets/Scripts/Animation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Anim : MonoBehaviour
+{
+    private void Update()
+    {
+    }
+
+
+    public void playAnimation()
+    {
+        Debug.Log("hy");
+        GetComponent<Animation>().Play("Jump In Place");
+        // StartCoroutine(delay());
+        //  GetComponent<Animation>().Play("jumping");
+
+        // GameObject obj = Instantiate(prefab) as GameObject;
+        // anim = obj.GetComponent<Animation>();
+    }
+
+    private void Stand()
+    {
+        GetComponent<Animation>().Play("Jump In Place");
+    }
+
+    private IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(2.5f);
+        // stand();
+    }
+}
---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject spawn;
+
+    private PlacementIndicator indicator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        indicator = FindObjectOfType<PlacementIndicator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            GameObject obj = Instantiate(spawn, indicator.transform.position, indicator.transform.rotation);
+        }
+    }
+
+    public void Activate()
+    {
+        GameObject obj = Instantiate(spawn, indicator.transform.position, indicator.transform.rotation);
+    }
+}
